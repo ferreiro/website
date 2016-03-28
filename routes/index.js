c79ec45f84ef05fcd85936c32d941c21c60ef2c8
Jorge Ferreiro <me@jgferreiro.com>
2016-03-28 07:15:47 +0200

    Updated /about with new icons

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,8 @@ router.get('/', function(req, res, next) {
 router.get('/about', function(req, res, next) {
   res.render('about', {
     title: 'About me',
-    path: 'about'
+    path: 'about',
+    content: content.about
   });
 });
 
