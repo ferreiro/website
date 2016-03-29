a075d54a3da53c53ca4b554e2b483b651a9ecc35
Jorge Ferreiro <me@jgferreiro.com>
2016-03-29 21:25:08 +0200

    Social tested

--- routes/social.js
+++ routes/social.js
@@ -1,32 +1,61 @@
 var express = require('express');
 var request = require("request");
+var GitHubApi = require("github");
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('social');
+  res.render(getGithubUser());
 });
 
-/*
-  Calling Github API and get total number of open
-  repositories I have hosted on my account
-*/
-function githubNumRepositories(profile) {
-  var total_repositories = 0;
-  var url = "https://api.github.com/users/" + profile;
+module.exports = router;
 
-  console.log(url);
-  request({
-      url: url,
-      json: true
-  }, function (error, response, body) {
-      if (!error && response.statusCode === 200) {
-          total_repositories = body.public_repos;
-          console.log("body.public_repos " + total_repositories); // Print the json response
-          console.log("The respond is " + body); // Print the json response
+function getGithubUser() {
+  var github = new GitHubApi({
+      // required
+      version: "3.0.0",
+      // optional
+      debug: true,
+      protocol: "https",
+      host: "ferreiro.me", // should be api.github.com for GitHub
+      pathPrefix: "/api/v3", // for some GHEs; none for GitHub
+      timeout: 5000,
+      headers: {
+          "user-agent": "My-Cool-GitHub-App" // GitHub is happy with a unique user agent
       }
-      return total_repositories;
   });
-  return total_repositories;
+
+  github.user.getFollowingFromUser({
+      // optional:
+      // headers: {
+      //     "cookie": "blahblah"
+      // },
+      user: "ferreiro"
+  }, function(err, res) {
+      console.log(JSON.stringify(res));
+      return JSON.stringify(res);
+  });
 }
 
-module.exports = router;
+//
+// /*
+//   Calling Github API and get total number of open
+//   repositories I have hosted on my account
+// */
+// function githubNumRepositories(profile) {
+//   var total_repositories = 0;
+//   var url = "https://api.github.com/users/" + profile;
+//
+//   console.log(url);
+//   request({
+//       url: url,
+//       json: true
+//   }, function (error, response, body) {
+//       if (!error && response.statusCode === 200) {
+//           total_repositories = body.public_repos;
+//           console.log("body.public_repos " + total_repositories); // Print the json response
+//           console.log("The respond is " + body); // Print the json response
+//       }
+//       return total_repositories;
+//   });
+//   return total_repositories;
+// }
