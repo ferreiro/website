95791fff305864685330e06eb2805e3c011d1fee
Jorge Ferreiro <me@jgferreiro.com>
2016-03-29 03:19:22 +0200

    Updated manifest and favicons

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public','images','favicons','favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
