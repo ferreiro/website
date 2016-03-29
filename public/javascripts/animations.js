a8c6504d3db6b0158025a3be84946eb7bacee156
Jorge Ferreiro <me@jgferreiro.com>
2016-03-29 19:01:09 +0200

    Improved a bit site load speed

--- public/javascripts/animations.js
+++ public/javascripts/animations.js
@@ -3,35 +3,35 @@ var footer = $('footer');
 var sectionHeader = $('#sectionHeader');
 var ss = sessionStorage;
 
-var animations = [ "zoomIn", "fadeInUpBig", "rollIn" ]; //  'flipInY', '', 'bounceIn'
+var animations = [ "zoomIn", "fadeInUpBig"]; //  'flipInY', '', 'bounceIn'
 
-function animateMainContainer() {
+window.animateMainContainer = function animateMainContainer() {
     var mainContainer = $('#animate_div');
     var randomAnimation = Math.floor((Math.random() * animations.length));
-
-    if (footer.length) {
-      footer.hide(0).delay(1200).show(0);
-    }
 
     if (ss.disabledMenuEffect != "true" && menu.length) {
       ss.disabledMenuEffect = "true"; // Don't repeat the header effect every load.
       menu.addClass('animated zoomIn');
     }
-    if (sectionHeader.length) {
-      sectionHeader.hide(0).delay(300).show(0).addClass('animated flipInX');
+    else {
+      menu.removeClass('animated zoomIn');
     }
+
+    if (footer.length) {
+      footer.hide(0).delay(1200).show(0);
+    }
+
+    if (sectionHeader.length) {
+      sectionHeader.addClass('animated flipInX');
+    }
+
     if (mainContainer.length) {
-      mainContainer.hide(0).delay(900).show(0).addClass('animated ' + animations[randomAnimation]);
+      mainContainer.addClass('animated ' + animations[randomAnimation]);
     }
 }
 
-$(document).ready(function(){
-  animateMainContainer();
-  animate();
-});
-
 // Animate call to action button each 3 seconds
-function animate() {
+window.animateCalltoAction = function animate() {
   var startTime = 2000;
   var animation = "animated bounce";
   var cButton = $('.callToAction');
