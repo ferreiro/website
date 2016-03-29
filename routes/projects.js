56731f34f5145810b5b02a12463a143ad0a939b2
Jorge Ferreiro <me@jgferreiro.com>
2016-03-29 03:44:00 +0200

    Contact routes refactored

--- routes/projects.js
+++ routes/projects.js
@@ -2,16 +2,18 @@ var express = require('express');
 var content = require('../public/content/english.json'); // TODO: Add multilanguage
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', projects);
+
+// router.get('/dotfiles', function(req, res, next) {
+//   res.send('Dotfiles project page');
+// });
+
+function projects(req, res, next) {
   res.render('projects', {
     title: 'Projects &amp; Works',
     path: 'projects',
     content: content.projects
   });
-});
-
-router.get('/dotfiles', function(req, res, next) {
-  res.send('Dotfiles project page');
-});
+}
 
 module.exports = router;
