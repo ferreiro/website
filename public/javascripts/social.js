69bd7bcd0a8afa66883cab37370e7c6be11c9acb
Jorge Ferreiro <me@jgferreiro.com>
2016-04-02 16:22:12 +0200

    Updated Github repositories automatically from API

---
+++ public/javascripts/social.js
@@ -0,0 +1,23 @@
+var githubRepostiries = $('#githubRespositories');
+
+function updateGithubRepositories() {
+  var apiEndPoint = 'social/github';
+
+  $.ajax({
+    url: apiEndPoint,
+    type: 'GET',
+    dataType: 'json',
+    encode: true
+  })
+  .done(function(apiObject) {
+    var public_repos = apiObject.public_repos;
+    githubRepostiries.html(public_repos); // Update github repositories
+  })
+  .fail(function(apiObject) {
+    console.log('footer.js > Fail to load number of repositories');
+  })
+  .always(function(apiObject) {
+  });
+}
+
+updateGithubRepositories();
--- routes/social.js
+++ routes/social.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var request = require("request");
-var GitHubApi = require("github");
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
@@ -10,116 +9,38 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/github', githubFollowers);
+
 module.exports = router;
 
-//
-// function github(req, res, next) {
-//   var name = 'ferreiro';
-//   var url = "https://api.github.com/users/" + name;
-//   var timeoutInMilliseconds = 10*1000;
-//
-//   var opts = {
-//     url: url,
-//     headers: {
-//       'User-Agent': 'request'
-//     },
-//     timeout: timeoutInMilliseconds
-//   };
-//
-//   res.setHeader('Content-Type', 'application/json');
-//
-//   request(opts, function (err, response, user) {
-//
-//     user = JSON.parse(user); // Important -->
-//
-//     if (err) {
-//       res.json({
-//           "public_repos": -1
-//       });
-//     }
-//     res.json({
-//         "public_repos": user.public_repos // We return form object we created before
-//     });
-//     // var statusCode = res.statusCode
-//   };
+///////////////////
+//// FUNCTIONS ////
+///////////////////
 
-// router.get('/github', function(req, res, next) {
-//     var name = 'ferreiro';
-//     var url = "https://api.github.com/users/" + name;
-//     var timeoutInMilliseconds = 10*1000;
-//
-//     var opts = {
-//       url: url,
-//       headers: {
-//         'User-Agent': 'request'
-//       },
-//       timeout: timeoutInMilliseconds
-//     };
-//
-//     res.setHeader('Content-Type', 'application/json');
-//
-//     request(opts, function (err, response, user) {
-//
-//       user = JSON.parse(user); // Important -->
-//
-//       if (err) {
-//         res.json({
-//             "public_repos": -1
-//         });
-//       }
-//
-//
-//       res.json({
-//           "public_repos": user.public_repos // We return form object we created before
-//       });
-//       // var statusCode = res.statusCode
-//
-//
-//
-//     });
-// });
+function githubFollowers(req, res, next) {
+  var username = 'ferreiro';
+  var url = 'https://api.github.com/users/' + username;
+  var timeoutInMilliseconds = 10*1000;
+  var opts = {
+    url: url,
+    headers: {
+      'User-Agent': 'request'
+    },
+    timeout: timeoutInMilliseconds
+  };
 
+  res.setHeader('Content-Type', 'application/json');
 
-//
-// router.get('/github/repositories', function(req, res, next) {
-//   var name = 'name';
-//   var user = getGithubUser(name);
-//   var totalRepositories = -1;
-//
-//   console.log(user);
-//
-//   if (user.public_repos) {
-//     totalRepositories = user.public_repos;
-//   }
-//
-//   res.setHeader('Content-Type', 'application/json');
-//   res.json({
-//       "public_repos": totalRepositories // We return form object we created before
-//   });
-// });
-//
-// module.exports = router;
-//
-// function getGithubUser(name) {
-//   var user = {};
-//   var url = "https://api.github.com/users/" + name;
-//   var timeoutInMilliseconds = 10*1000;
-//
-//   var opts = {
-//     url: url,
-//     headers: {
-//       'User-Agent': 'request'
-//     },
-//     timeout: timeoutInMilliseconds
-//   };
-//
-//   request(opts, function (err, res, user) {
-//     console.log(err);
-//     if (err) {
-//       console.dir(err);
-//       return {};
-//     }
-//     // var statusCode = res.statusCode
-//     return user;
-//   });
-// }
+  request(opts, function (err, response, user) {
+    user = JSON.parse(user);
+    public_repos = user.public_repos;
+
+    if (err) {
+      public_repos = null;
+    }
+
+    res.json({
+      "public_repos": public_repos // We return form object we created before
+    });
+  });
+}
