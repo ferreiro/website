75fa046ea338f7ff6f82f0e0745afe18ae27d685
Jorge Ferreiro <jorge@ferreiro.me>
2016-05-29 19:50:14 +0200

    Hide powered-by

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Hide Powered-by
+app.disable('x-powered-by');
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(favicon(path.join(__dirname, 'public','images','favicons','favicon.ico')));
