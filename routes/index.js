3c07e596e3de4600147f46b797661977e7e4adc7
Jorge Ferreiro <me@jgferreiro.com>
2016-09-24 22:13:33 +0200

    Updated home route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var content = require('../public/content/english.json'); // TODO: Add multilanguage
 var router = express.Router();
 
-router.get('/', about); // Home shows about page
+router.get('/', home); // Home shows about page
 router.get('/about', about);
 router.get('/university', university);
 router.get('/stats', stats);
@@ -12,10 +12,10 @@ module.exports = router;
 // FUNCTIONS
 
 function home(req, res, next) {
-  res.render('index', {
+  res.render('about', {
     title: 'Home',
-    path: 'index',
-    content: content.home
+    path: 'featured',
+    content: content.about
   });
 }
 
