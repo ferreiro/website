06b92f00a4636adf486ca92878acb48cd3f3afe1
Jorge Ferreiro <me@jgferreiro.com>
2016-09-24 21:58:30 +0200

    Fixed - Crashed application if API call fails

--- routes/social.js
+++ routes/social.js
@@ -11,29 +11,28 @@ module.exports = router;
 ///////////////////
 
 function githubFollowers(req, res, next) {
-  var username = 'ferreiro';
-  var url = 'https://api.github.com/users/' + username;
-  var timeoutInMilliseconds = 10*1000;
+  var username = 'ferreiro'
+  var url = 'https://api.github.com/users/' + username
+  var timeoutInMilliseconds = (10*1000)
   var opts = {
     url: url,
     headers: {
       'User-Agent': 'request'
     },
     timeout: timeoutInMilliseconds
-  };
-
-  res.setHeader('Content-Type', 'application/json');
+  }
 
   request(opts, function (err, response, user) {
-    user = JSON.parse(user);
-    public_repos = user.public_repos;
+    var public_repos = null
 
-    if (err) {
-      public_repos = null;
+    if (!err) {
+      var user = JSON.parse(user)
+      public_repos = user.public_repos
     }
 
+    res.setHeader('Content-Type', 'application/json')
     res.json({
       "public_repos": public_repos // We return form object we created before
-    });
-  });
+    })
+  })
 }
