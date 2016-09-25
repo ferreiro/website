d84eadb10695fe3623f2219d072c70d7c351c323
Jorge Ferreiro <me@jgferreiro.com>
2016-09-25 11:02:39 +0200

    Added home

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var content = require('../public/content/english.json'); // TODO: Add multilanguage
 var router = express.Router();
 
-router.get('/', about); // Home shows about page
+router.get('/', home); // Home shows about page
 router.get('/about', about);
 router.get('/university', university);
 router.get('/stats', stats);
@@ -12,7 +12,7 @@ module.exports = router;
 // FUNCTIONS
 
 function home(req, res, next) {
-  res.render('about', {
+  res.render('home', {
     title: 'Home',
     path: 'featured',
     content: content.about
