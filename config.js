d36eb866bfc65f8cdd873401531654f6451ae3dc
Jorge Ferreiro <me@jgferreiro.com>
2016-09-27 11:11:31 +0200

    Added config file

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {
+  "gmail": {
+    "USER": process.env.GMAIL_USER,
+    "PASS": process.env.GMAIL_PASSWD
+  },
+  "recaptcha": {
+    "PUBLIC_KEY": process.env.RECAPTCHA_PUBLIC,
+    "SECRET_KEY": process.env.RECAPTCHA_SECRET
+  }
+}
+
+module.exports = config
