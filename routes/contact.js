8f25e257d64effea36972563e5f81dc34bf0b890
Jorge Ferreiro <me@jgferreiro.com>
2016-10-01 02:28:34 +0200

    Updated npm start with nodemon + new contact

--- routes/contact.js
+++ routes/contact.js
@@ -37,8 +37,7 @@ function feedback(req, res, next) {
   })
 }
 
-function submitForm (req, res) {
-  var validForm
+function submitForm (req, res, next) {
   var form = {
     'name': req.body.contact_name || undefined,
     'email': req.body.contact_email || undefined,
@@ -49,45 +48,52 @@ function submitForm (req, res) {
 
   validForm = validateForm(form.email, form.message)
 
-  if (validForm) { // validCaptcha &&
-    // transporter = nodemailer.createTransport({
-    //     service: 'Gmail',
-    //     auth: {
-    //         user: GMAIL_USER || undefined,
-    //         pass: GMAIL_PASS || undefined
-    //     }
-    // })
+  if (validForm) {
 
-    transporter = nodemailer.createTransport(
-      'smtps://' + GMAIL_USER + ':' + GMAIL_PASS + '@smtp.gmail.com');
+    var endpoint
+    var transporter
+    var compiledTemplate
+    var htmlEmail
+    var mailOptions
 
-    var compiledTemplate = pug.compileFile('views/emailTemplate.pug')
-    var htmlEmail = compiledTemplate({
-      name: form.name,
-      email: form.email,
-      message: form.message,
-      subscribed: form.subscribed,
-      source: form.source
-    })
+    endpoint = 'smtps://' + GMAIL_USER + '%40gmail.com:' + GMAIL_PASS + '@smtp.gmail.com'
+    transporter = nodemailer.createTransport(endpoint)
 
-    mailOptions = { // Setup e-mail data with unicode symbols
-        subject: '[Ferreiro/contact] Message from ' + form.name,
-        from: String(form.name) + ' <' + String(form.email) + '>', // sender address
-        to: 'jorge@ferreiro.me', // list of receivers
-        replyTo: form.email,
-        html: htmlEmail // html body
+    // HTML Template
+    compiledTemplate = pug.compileFile('views/emailTemplate.pug')
+    htmlEmail = compiledTemplate({ form: form })
+    console.log(endpoint);
+
+    // Setup e-mail data with unicode symbols
+    mailOptions = {
+      from: '"${form.name} 👥" <${form.email}>', // sender address
+      to: '${GMAIL_USER}@gmail.com', // list of receivers
+      subject: 'Hello ✔', // Subject line
+      text: 'Hello world 🐴', // plaintext body
+      html: '<b>Hello world 🐴</b>' // html body
     }
-    transporter.sendMail (mailOptions, function(error, data) {
-      res.setHeader('Content-Type', 'application/json')
-      res.json({
-        'error': error,
-        'validData': true,
-        'emailSent': (!error)
-      })
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          res.json({
+            'data': {
+              'error': true,
+              'validData' : validForm,
+              'emailSent' : false
+            }
+          })
+        }
+        else {
+          res.json({
+            'error': error,
+            'validData': true,
+            'emailSent': (!error)
+          })
+        }
     })
   }
   else {
-    res.setHeader('Content-Type', 'application/json')
     res.json({
       'data': {
         'error': true,
@@ -97,6 +103,60 @@ function submitForm (req, res) {
     })
   }
 }
+
+
+// function submitForm (req, res) {
+//   var validForm
+//   var form = {
+//     'name': req.body.contact_name || undefined,
+//     'email': req.body.contact_email || undefined,
+//     'message': req.body.contact_msg || undefined,
+//     'subscribed': req.body.contact_newsletter || false,
+//     'source': req.body.contact_source || 'General'
+//   }
+//
+//   validForm = validateForm(form.email, form.message)
+//
+//   if (validForm) { // validCaptcha &&
+//     transporter = nodemailer.createTransport(
+//       'smtps://' + GMAIL_USER + ':' + GMAIL_PASS + '@smtp.gmail.com');
+//
+//     var compiledTemplate = pug.compileFile('views/emailTemplate.pug')
+//     var htmlEmail = compiledTemplate({
+//       name: form.name,
+//       email: form.email,
+//       message: form.message,
+//       subscribed: form.subscribed,
+//       source: form.source
+//     })
+//
+//     mailOptions = { // Setup e-mail data with unicode symbols
+//         subject: '[Ferreiro/contact] Message from ' + form.name,
+//         from: String(form.name) + ' <' + String(form.email) + '>', // sender address
+//         to: 'jorge@ferreiro.me', // list of receivers
+//         replyTo: form.email,
+//         html: htmlEmail // html body
+//     }
+//     transporter.sendMail (mailOptions, function(error, data) {
+//       res.setHeader('Content-Type', 'application/json')
+//       res.json({
+//         'error': error,
+//         'validData': true,
+//         'emailSent': (!error)
+//       })
+//     })
+//   }
+//   else {
+//     res.setHeader('Content-Type', 'application/json')
+//     res.json({
+//       'data': {
+//         'error': true,
+//         'validData' : validForm,
+//         'emailSent' : false
+//       }
+//     })
+//   }
+// }
 
 validateForm = function (email, message) {
   var isEmailCorrect = validator.isEmail(String(email))
