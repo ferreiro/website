d3b94eb231e3152b3351875ef3792c97a23638c1
Jorge Ferreiro <jorge@ferreiro.me>
2016-10-01 21:10:45 +0200

    Added api status

--- api/index.js
+++ api/index.js
@@ -7,6 +7,11 @@ app.set('view engine', 'jade')
 
 // API Route
 app.use('/v1', require('./v1'))
+app.get('/status', function (req, res) {
+  res.status(200).json({
+    'response': 'Everything is working correctly'
+  })
+})
 app.all('*', errors.endpointNotFound)
 
 module.exports = app
