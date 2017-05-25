77e236d7b39feb0906f049e0ba89032ffb8ac017
Jorge Ferreiro <jorge@ferreiro.me>
2017-05-25 15:50:08 +0200

    Hello helmet

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-require('newrelic')
+if (process.env.NODE_ENV === 'production') {
+  require('newrelic') // Stats for production only
+}
 
 var express = require('express')
 var path = require('path')
@@ -6,6 +8,7 @@ var favicon = require('serve-favicon')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
+var helmet = require('helmet')
 
 var routes = require('./routes/index')
 var projects = require('./routes/projects')
@@ -18,8 +21,8 @@ var app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
 
-// Hide Powered-by
-app.disable('x-powered-by')
+// Add security layer
+app.use(helmet())
 
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicons', 'favicon.ico')))
