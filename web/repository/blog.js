ed05d37c6ea70ad25885721fd08f4a13d3d77e00
Jorge Ferreiro <jorge@ferreiro.me>
2017-09-30 00:13:33 +0200

    Get most recent and random posts methods

--- web/repository/blog.js
+++ web/repository/blog.js
@@ -5,6 +5,43 @@ module.exports.create = function (postData) {
   return newPost.save()
 }
 
+// https://stackoverflow.com/questions/5539955/how-to-paginate-with-mongoose-in-node-js
+module.exports.getMostRecentPosts = function (opts) {
+  const limit = opts.limit || 3
+  const postPermalinkToSkip = opts.postPermalinkToSkip || ''
+
+  const query = {
+    created: { $lte: new Date() },
+    published: true
+  }
+  return Post.find(query)
+    .limit(limit)
+    .sort({ created: -1 })
+}
+
+module.exports.getRandomPosts = function (opts) {
+  const limit = opts.limit || 4
+  const postPermalinkToSkip = opts.postPermalinkToSkip || ''
+
+  return new Promise((resolve, reject) => {
+    const filter = {
+      published: true,
+      permalink: { $ne: postPermalinkToSkip }
+    }
+    const fields = {}
+    const options = {
+      limit: limit
+    }
+    Post.findRandom(filter, fields, options, (err, posts) => {
+      if (err) {
+        return reject(err)
+      }
+      return resolve(posts)
+    })
+  })
+}
+
+
 module.exports.getAllPublished = function (options) {
   return Post.find({
     'published': true
