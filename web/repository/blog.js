dce2e5fa1f4898145a321ce12dbc8ec982a27c44
Jorge Ferreiro <jorge@ferreiro.me>
2017-09-30 05:16:10 +0200

    Improved and new methods on blog repository

--- web/repository/blog.js
+++ web/repository/blog.js
@@ -5,22 +5,79 @@ module.exports.create = function (postData) {
   return newPost.save()
 }
 
+module.exports.findByPermalink = function (query) {
+  return Post.findOne({
+    permalink: query.permalink
+  })
+}
+
+module.exports.findById = function findById (query) {
+  return Post.findOne({
+    _id: query.id
+  })
+}
+
+module.exports.findAndUpdateByPermalink = function (postPermalink, postData) {
+  return Post.findOneAndUpdate({
+    permalink: postPermalink
+  },postData, { new: true })
+}
+
+module.exports.findAndDeleteByPermalink = function (postPermalink) {
+  return Post.findOneAndRemove({
+    permalink: postPermalink
+  })
+}
+
+module.exports.getAll = function () {
+  return Post.find()
+    .sort({ createdAt: -1 })
+}
+
+module.exports.getAllDrafts = function (options) {
+  return Post.find({
+    'published': false
+  }).sort({ createdAt: -1 })
+}
+
+module.exports.getAllPublished = async function (opts) {
+  var maxLimit = 10
+
+  if (opts) {
+    if (opts.maxPagePosts) {
+      const newLimit = opts.maxPagePosts
+      maxLimit = newLimit < maxLimit ? newLimit : maxLimit
+    }
+  }
+
+  const query = {
+    'published': true
+  }
+  const options = {
+    sort: { createdAt: -1 },
+    lean: true,
+    page: opts.nextPage || 1,
+    limit: maxLimit
+  }
+  return Post.paginate(query, options)
+}
+
 // https://stackoverflow.com/questions/5539955/how-to-paginate-with-mongoose-in-node-js
 module.exports.getMostRecentPosts = function (opts) {
   const limit = opts.limit || 3
   const postPermalinkToSkip = opts.postPermalinkToSkip || ''
 
   const query = {
-    created: { $lte: new Date() },
+    createdAt: { $lte: new Date() },
     published: true
   }
   return Post.find(query)
     .limit(limit)
-    .sort({ created: 1 })
+    .sort({ createdAt: -1 })
 }
 
 module.exports.getRandomPosts = function (opts) {
-  const limit = opts.limit || 4
+  const limit = opts.limit || 3
   const postPermalinkToSkip = opts.postPermalinkToSkip || ''
 
   return new Promise((resolve, reject) => {
@@ -40,40 +97,3 @@ module.exports.getRandomPosts = function (opts) {
     })
   })
 }
-
-
-module.exports.getAllPublished = function (options) {
-  return Post.find({
-    'published': true
-  }).sort({ created: 1 })
-}
-
-module.exports.getAllDrafts = function (options) {
-  return Post.find({
-    'published': false
-  }).sort({ created: 1 })
-}
-
-module.exports.getAll = function () {
-  return Post.find().sort({ created: 1 })
-}
-
-module.exports.findByPermalink = function (query) {
-  return Post.findOne({
-    permalink: query.permalink
-  })
-}
-
-module.exports.findAndUpdateByPermalink = function (postPermalink, postData) {
-  return Post.findOneAndUpdate({
-    permalink: postPermalink
-  },postData, { new: true })
-}
-
-module.exports.findAndDeleteByPermalink = function (postPermalink) {
-  return Post.findOneAndRemove({
-    permalink: postPermalink
-  })
-}
-
-
