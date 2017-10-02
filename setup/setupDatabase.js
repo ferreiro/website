194e00ae096e13ff3be4d098298bb7a1e5c3d5be
Jorge Ferreiro <jorge@ferreiro.me>
2017-10-02 08:28:06 +0200

    Mongoose bluebird promise

--- setup/setupDatabase.js
+++ setup/setupDatabase.js
@@ -1,11 +1,13 @@
 const debug = require('debug')('ferreiro:setup:database')
 const mongoose = require('mongoose');
+const bluebird = require('bluebird')
 
 const env = require('../env')
 const mongoDB = env.MONGODB_URI
 
 module.exports = () => {
   //Set up default mongoose connection
+  mongoose.Promise = bluebird
   mongoose.connect(mongoDB, {
     useMongoClient: true
   }, (error) => {
