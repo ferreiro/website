f8108212dc7478c90fd6687eb226bfd1b9aee951
Jorge Ferreiro <jorge@ferreiro.me>
2017-10-02 08:36:13 +0200

    Added AWS to env variables

--- env/development.js
+++ env/development.js
@@ -6,6 +6,12 @@ module.exports = {
   ADMIN_PASS: 'admin',
   MAILCHIMP_API_TOKEN: process.env.MAILCHIMP_API_TOKEN,
 
+  // AMAZON WEB SERVICES
+  S3_REGION: process.env.S3_REGION,
+  S3_BUCKET: process.env.S3_BUCKET,
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+
   // Email Services
   CONTACT_EMAIL: process.env.CONTACT_EMAIL,
   MAILGUN_USER: process.env.MAILGUN_USER,
