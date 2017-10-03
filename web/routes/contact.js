e9e3e9fe6ab746d89a19855492ba51e64c671358
Jorge Ferreiro <jorge@ferreiro.me>
2017-10-03 09:20:29 +0200

    New talks contact form

--- web/routes/contact.js
+++ web/routes/contact.js
@@ -4,16 +4,17 @@
 const express = require('express')
 const router = express.Router()
 
-const content = require('../content/english/contact.json')
-
 // Routes
 router.get('/', contact)
+router.get('/talk', contactTalk)
 router.get('/feedback', feedback)
 
 module.exports = router
 
 // FUNCTIONS
+
 function contact (req, res, next) {
+  let content = require('../content/english/contact.json')
   res.render('contact', {
     title: 'Contact me',
     path: 'contact',
@@ -21,10 +22,23 @@ function contact (req, res, next) {
   })
 }
 
-function feedback(req, res, next) {
+function contactTalk (req, res, next) {
+  let content = require('../content/english/contact.json')
+  content.config.claim = "Are you organizing an event and want me to be your speaker? Send me an email with the proposal (<a class='email openModalBox'>jorge at ferreiro dot me</a>). I'll be happy to be part of it!"
+  content.sendButton = "Submit proposal"
+  content.form.message = "What's your proposal? Name, dates, topics, etc..."
   res.render('contact', {
-    title: 'Feeback'
-    , path: 'feedback'
-    , content: content.feedback
+    title: 'Jorge at your event',
+    path: 'talks',
+    content: content
+  })
+}
+
+function feedback(req, res, next) {
+  const content = require('../content/english/contact.json')
+  res.render('contact', {
+    title: 'Feeback',
+    path: 'feedback',
+    content: content.feedback
   })
 }
