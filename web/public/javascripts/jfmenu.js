998f5db1f499ce4c3bb57865fae48d3691e3ee5a
Jorge Ferreiro <jorge@ferreiro.me>
2017-10-05 03:33:20 +0200

    Close mobile menu and scrolltop on desktop when clicked on current section

--- web/public/javascripts/jfmenu.js
+++ web/public/javascripts/jfmenu.js
@@ -1,61 +1,95 @@
+setupDesktopMenu()
+setupMobileMenu()
 
-// COPYRIGHT Jorge ferreiro
+function setupDesktopMenu () {
+  var menu = $('.menu__nav')
+  menu.find('a').click(function(event) {
+    var menuItem = $(this)
 
-var body = $('body');
-var jfmenu = $('.jfmenu');
-var jfmenuContent = $('.jfmenu_content');
-var jfmenuOverlay = $('.jfmenu_overlay');
-var menuButton = $('.jfmenu__mobileButton');
+    // When click on current section.
+    // Scroll to top.
+    if (menuItem.hasClass('item-selected')) {
+      event.preventDefault()
 
-jfmenuOverlay.click(function() {
-  if ($('.jfmenu').hasClass('jfmenu_displayed')) {
-    hideMenu();
+      scrollTop({
+        position: 0,
+        speed: 300
+      })
+    }
+  })
+}
+
+function setupMobileMenu () {
+  var body = $('body')
+  var jfmenu = $('.jfmenu')
+  var jfmenuContent = $('.jfmenu_content')
+  var jfmenuOverlay = $('.jfmenu_overlay')
+  var menuButton = $('.jfmenu__mobileButton')
+
+  jfmenuOverlay.click(function() {
+    if ($('.jfmenu').hasClass('jfmenu_displayed')) {
+      hideMenu()
+    }
+  })
+
+  // Actions: if click on a menu item which is the current section
+  // hide the menu. If click on a item with a submenu, then open it.
+  jfmenu.find('li').click(function(event) {
+    var e = $(this)
+    var submenu_list = e.find('ul')
+
+    var hasClickedOnCurrentPage = e.hasClass('current_section')
+    if (hasClickedOnCurrentPage) {
+      event.preventDefault() // Don't open url again.
+      hideMenu()
+    }
+
+    if (submenu_list.length > 0) {
+      submenu_list.each(function() {
+        var menu = $(this)
+        menu.toggleClass('jfmenu_displayed') // Display the submenu
+      })
+    }
+  })
+
+
+  menuButton.click(function() {
+    var e = $(this)
+
+    if (jfmenu.hasClass('jfmenu_displayed')) {
+      hideMenu()
+    }
+    else {
+      displayMenu()
+    }
+  })
+
+  function hideMenu () {
+    if (jfmenu.hasClass('jfmenu_displayed')) {
+      body.removeClass('body_overflow')
+      jfmenu.removeClass('jfmenu_displayed')
+      jfmenuOverlay.removeClass('fadeIn')
+      jfmenuContent.removeClass('fadeInDownBig')
+      menuButton.removeClass('jfmenu__mobileButton__active')
+    }
   }
-});
 
-// Menu links.
-// When the menu link has a submenu inside (aka <ul>), display the submenu
-// in onther case, don't do anaything
-$('.jfmenu').find('li').click(function(event) {
-  var e = $(this);
-  var submenu_list = e.find('ul');
-
-  if (submenu_list.length > 0) {
-    submenu_list.each(function() {
-      var menu = $(this);
-      menu.toggleClass('jfmenu_displayed'); // Display the submenu
-    });
-  }
-});
-
-
-menuButton.click(function() {
-  var e = $(this);
-
-  if (jfmenu.hasClass('jfmenu_displayed')) {
-    hideMenu();
-  }
-  else {
-    displayMenu();
-  }
-});
-
-function hideMenu(){
-  if (jfmenu.hasClass('jfmenu_displayed')) {
-    body.removeClass('body_overflow');
-    jfmenu.removeClass('jfmenu_displayed');
-    jfmenuOverlay.removeClass('fadeIn');
-    jfmenuContent.removeClass('fadeInDownBig');
-    menuButton.removeClass('jfmenu__mobileButton__active');
+  function displayMenu () {
+    if (!jfmenu.hasClass('jfmenu_displayed')) {
+      body.addClass('body_overflow')
+      jfmenu.addClass('jfmenu_displayed')
+      jfmenuOverlay.addClass('animated fadeIn')
+      jfmenuContent.addClass('animated fadeInDownBig')
+      menuButton.addClass('jfmenu__mobileButton__active')
+    }
   }
 }
 
-function displayMenu(){
-  if (!jfmenu.hasClass('jfmenu_displayed')) {
-    body.addClass('body_overflow');
-    jfmenu.addClass('jfmenu_displayed');
-    jfmenuOverlay.addClass('animated fadeIn');
-    jfmenuContent.addClass('animated fadeInDownBig');
-    menuButton.addClass('jfmenu__mobileButton__active');
-  }
+function scrollTop (opts) {
+  const position = opts && opts.position ? opts.position : 0
+  const speed = opts && opts.speed ? opts.speed : 300
+
+  $('html, body').animate({
+    scrollTop: opts && opts.position
+  }, speed)
 }
