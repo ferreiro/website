06e46b0bee3d7c56682de4f304ab97e231fbd293
Jorge Ferreiro <jorge@ferreiro.me>
2017-10-09 20:29:47 +0200

    Removed newrelic from app

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-if (process.env.NODE_ENV === 'production') {
-  require('newrelic') // Stats for production only
-}
-
 const setupApp = require('./setup')
 const express = require('express')
 const app = express()
