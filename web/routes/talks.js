6d9154978b8d9bd53e45988753094e83e98856f3
Jorge Ferreiro <jorge@ferreiro.me>
2017-10-18 05:29:58 +0200

    Fixed talks og:image url

--- web/routes/talks.js
+++ web/routes/talks.js
@@ -10,7 +10,7 @@ module.exports = router
 function getTalks (req, res, next) {
   res.render('talks', {
     title: 'Talks and Workshops',
-    image: '/images/talks/jorge_ferreiro_speaker_talks_workshops.jpg',
+    image: 'https://www.ferreiro.me/images/talks/jorge_ferreiro_speaker_talks_workshops.jpg',
     path: 'talks',
     config: talks.config,
     pastTalks: talks.pastTalks
