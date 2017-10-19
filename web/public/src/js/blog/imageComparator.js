32eae21a14f8b5ec403135f998d77a3e28299384
Jorge Ferreiro <jorge@ferreiro.me>
2017-10-19 03:36:43 +0200

    Fixed imageComparator: launch when every image has loaded

--- web/public/src/js/blog/imageComparator.js
+++ web/public/src/js/blog/imageComparator.js
@@ -1,5 +1,10 @@
 jQuery(document).ready(function($){
-  setupImageComparator()
+  // Wait until all images are loaded
+  // So the image comparator can calculate
+  // correctly the height
+  $(window).load(function () {
+    setupImageComparator()
+  })
 });
 
 function setupImageComparator () {
@@ -12,10 +17,12 @@ function setupImageComparator () {
     const imageBefore = images[0].src
     const imageAfter = images[1].src
 
-    // Create new html
+    // Create new imageComparator HTML entity
     const newComparatorHtml = comparatorTemplate(imageBefore, imageAfter)
     comparator.html(newComparatorHtml)
 
+    // Delay the creation of comparator
+    // So the height is well calculated.
     // Initiate the comparators
     $(".imageComparator__int").twentytwenty({
       // default_offset_pct: 0.7,
@@ -24,7 +31,7 @@ function setupImageComparator () {
   })
 }
 
-const comparatorTemplate = function (imageA, imageB) {
+function comparatorTemplate (imageA, imageB) {
   return `
   <div class="imageComparator">
     <div class="imageComparator__int">
