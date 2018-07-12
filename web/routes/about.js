8392fbd9eae1bf82c608544ebc6f61e254addbaf
Jorge Ferreiro <jorge@ferreiro.me>
2018-07-12 10:14:01 +0200

    Moved bio to about.json

--- web/routes/about.js
+++ web/routes/about.js
@@ -4,16 +4,18 @@ const router = express.Router()
 router.get('/', function (req, res, next) {
   const about = require('../content/english/about.json')
 
-  const education = about.education
+  const bio = about.bio
   const work = about.work
   const skills = about.skills
+  const education = about.education
 
   res.render('about', {
     title: 'Biography - Jorge Ferreiro',
     path: 'about',
+    bio: bio,
+    work: work,
     skills: skills,
-    education: education,
-    work: work
+    education: education
   })
 })
 
