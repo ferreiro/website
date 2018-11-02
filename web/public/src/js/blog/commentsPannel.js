76245ccb91ab39bb129e1707bb135ccd75f21861
Jorge Ferreiro <jorge@ferreiro.me>
2018-11-02 01:34:21 +0100

    Refactored comments into its own pannel

---
+++ web/public/src/js/blog/commentsPannel.js
@@ -0,0 +1,43 @@
+let alreadyLoadedCommments = false
+
+const commentsPannel = $('.comments-pannel');
+const closeCommentsPannelButton = $('.comments-pannel__close');
+const openCommentsPannelButton = $('.comments-pannel__open')
+
+openCommentsPannelButton.click(handleOpenCommentsPannel);
+closeCommentsPannelButton.click(handleCloseCommentsPannel);
+
+function handleOpenCommentsPannel (event) {
+    event.preventDefault();
+
+    commentsPannel.addClass('comments-pannel-displayed')
+
+    if (alreadyLoadedCommments === false) {
+        alreadyLoadedCommments = true
+        launchComments();
+    }
+}
+
+function handleCloseCommentsPannel (event) {
+    event.preventDefault();
+    commentsPannel.removeClass('comments-pannel-displayed')
+}
+
+/**
+ *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+ *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+var disqus_config = function () {
+    var PAGE_URL = document.getElementById('canonicalUrl').value
+    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+
+    var PAGE_IDENTIFIER = document.getElementById('postIdentifier').value
+    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+
+var launchComments = function () {
+    // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://ferreiro-1.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+}
