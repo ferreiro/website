fc4af4ce794bdbc159704302241f6dae38d18c7f
Jorge Ferreiro <jorge@ferreiro.me>
2019-06-06 00:47:50 +0200

    Moved menu into constants

--- packages/ferreiro-client/src/components/constants.js
+++ packages/ferreiro-client/src/components/constants.js
@@ -31,3 +31,50 @@ export const BUTTON_STYLE_NEUTRAL = 'neutral';
 export const BUTTON_SIZE_SMALL = 'small';
 export const BUTTON_SIZE_MEDIUM = 'medium';
 export const BUTTON_SIZE_BIG = 'big';
+
+// MENU
+export const MENU_ITEMS = [
+    {
+      "id": "home",
+      "hidden": false,
+      "icon": "ion-ios-home",
+      "name" : null,
+      "path" : "/",
+      "target": "_self"
+    },
+    {
+      "id": "about",
+      "icon": null,
+      "name" : "About me",
+      "path" : "/about",
+      "target": "_self"
+    },
+    {
+      "id": "blog",
+      "icon": null,
+      "name" : "Blog",
+      "path" : "/blog",
+      "target": "_self"
+    },
+    {
+      "id": "portfolio",
+      "icon": null,
+      "name" : "Portfolio",
+      "path" : "/portfolio",
+      "target": "_self"
+    },
+    {
+      "id": "talks",
+      "icon": null,
+      "name" : "Talks",
+      "path" : "/talks",
+      "target": "_self"
+    },
+    {
+      "id": "contact",
+      "icon": null,
+      "name" : "Contact",
+      "path" : "/contact",
+      "target": "_self"
+    }
+]
