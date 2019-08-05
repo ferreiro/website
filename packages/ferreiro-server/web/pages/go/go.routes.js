31c9f065ed379aafdb58ee1bc455bc40f4e844df
Jorge Ferreiro <jorge@ferreiro.me>
2019-08-05 23:59:20 +0200

    Adding Ian to go.routes

--- packages/ferreiro-server/web/pages/go/go.routes.js
+++ packages/ferreiro-server/web/pages/go/go.routes.js
@@ -8,6 +8,7 @@ const router = express.Router()
 const SHORT_URL_TO_URL = {
     dailyfocus: 'https://www.slideshare.net/jgferreiro/redis-76234580?qid=4408d51d-497d-44b1-8a7a-b688dbd51f83&v=&b=',
     perfomance: 'https://www.ferreiro.me/blog/speed-up-your-website-frontend-8-practical-tips',
+    ian: 'https://www.youtube.com/watch?v=R5Gzv7dwXKA&list=PLaN1b7vXPDt63YJVL2F7cmq7FZm1olKIn',
     rahma: 'https://www.youtube.com/playlist?list=PLaN1b7vXPDt6f3jYSdFbVeeEixCxWSepj',
     redis: 'https://www.slideshare.net/jgferreiro/redis-76234580?qid=4408d51d-497d-44b1-8a7a-b688dbd51f83&v=&b=',
     roundabout: 'https://speakerdeck.com/ferreiro/web-performance-jorge-ferreiro-twitter-london-js-roundabout',
