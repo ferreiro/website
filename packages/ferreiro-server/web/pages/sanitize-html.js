950175bebc1e00d1ac6822e0decacffbdd9ad788
Jorge Ferreiro <jorge@ferreiro.me>
2019-10-10 03:25:26 +0200

    Allowing mark html

--- packages/ferreiro-server/web/pages/sanitize-html.js
+++ packages/ferreiro-server/web/pages/sanitize-html.js
@@ -6,7 +6,7 @@ export const sanitizeHtml = (dirtyHtml) => {
             'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
             'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
             'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre',
-            'img', 'span', 'iframe', 'data-src'
+            'img', 'span', 'iframe', 'data-src', 'mark',
         ],
         allowedAttributes: {
             '*': [ 'id', 'href', 'align', 'alt', 'center', 'bgcolor' ],
@@ -16,7 +16,8 @@ export const sanitizeHtml = (dirtyHtml) => {
             h1: [ 'id' ],
             h2: [ 'id' ],
             ul: [ 'class' ],
-            iframe: [ 'width', 'height', 'src', 'frameborder', 'allow', 'allowfullscreen' ]
+            iframe: [ 'width', 'height', 'src', 'frameborder', 'allow', 'allowfullscreen' ],
+            mark: ['class', 'style'],
         }
     }
 
