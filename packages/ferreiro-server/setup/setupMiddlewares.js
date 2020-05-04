cf84a47ab365a1ab78ade1eea04aaf190d10bda9
Jorge Ferreiro <jorge@ferreiro.me>
2020-05-05 01:02:47 +0200

    Sitemap

--- packages/ferreiro-server/setup/setupMiddlewares.js
+++ packages/ferreiro-server/setup/setupMiddlewares.js
@@ -1,69 +1,117 @@
-const path = require('path')
-const favicon = require('serve-favicon')
-const logger = require('morgan')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const helmet = require('helmet')
-const session = require('express-session')
-const express = require('express')
-const compression = require('compression')
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const session = require("express-session");
+const express = require("express");
+const compression = require("compression");
+const SitemapGenerator = require("sitemap-generator");
+const cache = require("memory-cache");
 
-const env = require('../env')
+const CACHE_SITEMAP_KEY = "cached-sitemap";
+
+const env = require("../env");
 
 module.exports = (app) => {
-    // Add security layer
-    app.use(helmet())
+  // Add security layer
+  app.use(helmet());
 
-    // Compression!
-    app.use(compression())
+  // Compression!
+  app.use(compression());
 
-    // Search engines
-    app.get('/robots.txt', (req, res) => {
-        res.sendFile(path.join(__dirname + '/../robots.txt'))
-    })
-    app.get('/sitemap.xml', (req, res) => {
-        res.sendFile(path.join(__dirname + '/../sitemap.xml'))
-    })
+  // Search engines
+  app.get("/robots.txt", (req, res) => {
+    res.sendFile(path.join(__dirname + "/../robots.txt"));
+  });
 
-    // Serve static bower: http://goo.gl/e2nTBf
-    app.use(favicon(path.join(__dirname, '../web', 'public', 'src', 'images', 'favicons', 'favicon.ico'), {
-        maxAge: process.env.NODE_ENV === 'production' ? '2h' : 0
-    }))
-    app.use(express.static(path.join(__dirname, '../web/public/dst'), {
-        maxAge: process.env.NODE_ENV === 'production' ? '2h' : '10h'
-    }))
-    // NB: We generate this from the web client. It contains
-    // the react bundles...
-    app.use(express.static(path.join(__dirname, '../dst'), {
-        maxAge: process.env.NODE_ENV === 'production' ? '2h' : '10h'
-    }))
-    app.use('/bower_components', express.static(path.join(__dirname, '../bower_components'), {
-        maxAge: process.env.NODE_ENV === 'production' ? '2h' : 0
-    }))
-    app.use('/favicons', express.static(path.join(__dirname, '../web/public/src/images/favicons'), {
-        maxAge: process.env.NODE_ENV === 'production' ? '2h' : 0
-    }))
-    app.use(logger('dev'))
+  app.get("/sitemap.xml", (req, res) => {
+    const cachedSitemap = cache.get(CACHE_SITEMAP_KEY);
 
-    // order matters here for passport
-    app.use(cookieParser())
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({ extended: false }))
-
-    // Session configuration (always after previous block)
-    if (!env.SESSION_SECRET) {
-        throw new Error('SESSION_SECRET not provided ')
+    if (cachedSitemap) {
+      return res.send(cachedSitemap);
     }
-    app.use(session({
-        secret: env.SESSION_SECRET,
-        saveUninitialized: false,
-        resave: false
-    }))
 
-    // view engine setup
-    // NB: Since we're starting to use a duck structure, we'll point to the /web folder,
-    // Later on you, you can decide where to load the views using:
-    // Eg: return res.render('home/home.pug')
-    app.set('views', path.join(__dirname, '../web'))
-    app.set('view engine', 'pug')
-}
+    // create generator
+    const generator = SitemapGenerator("https://www.ferreiro.me/", {
+      stripQuerystring: true,
+    });
+
+    // register event listeners
+    generator.on("done", () => {
+      cache.put(CACHE_SITEMAP_KEY, latestVideo, CACHED_TIME_MS);
+      return res.sendFile(path.join(__dirname + "/../sitemap.xml"));
+    });
+
+    // start the crawler
+    generator.start();
+  });
+
+  // Serve static bower: http://goo.gl/e2nTBf
+  app.use(
+    favicon(
+      path.join(
+        __dirname,
+        "../web",
+        "public",
+        "src",
+        "images",
+        "favicons",
+        "favicon.ico"
+      ),
+      {
+        maxAge: process.env.NODE_ENV === "production" ? "2h" : 0,
+      }
+    )
+  );
+  app.use(
+    express.static(path.join(__dirname, "../web/public/dst"), {
+      maxAge: process.env.NODE_ENV === "production" ? "2h" : "10h",
+    })
+  );
+  // NB: We generate this from the web client. It contains
+  // the react bundles...
+  app.use(
+    express.static(path.join(__dirname, "../dst"), {
+      maxAge: process.env.NODE_ENV === "production" ? "2h" : "10h",
+    })
+  );
+  app.use(
+    "/bower_components",
+    express.static(path.join(__dirname, "../bower_components"), {
+      maxAge: process.env.NODE_ENV === "production" ? "2h" : 0,
+    })
+  );
+  app.use(
+    "/favicons",
+    express.static(path.join(__dirname, "../web/public/src/images/favicons"), {
+      maxAge: process.env.NODE_ENV === "production" ? "2h" : 0,
+    })
+  );
+  app.use(logger("dev"));
+
+  // order matters here for passport
+  app.use(cookieParser());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+
+  // Session configuration (always after previous block)
+  if (!env.SESSION_SECRET) {
+    throw new Error("SESSION_SECRET not provided ");
+  }
+  app.use(
+    session({
+      secret: env.SESSION_SECRET,
+      saveUninitialized: false,
+      resave: false,
+    })
+  );
+
+  // view engine setup
+  // NB: Since we're starting to use a duck structure, we'll point to the /web folder,
+  // Later on you, you can decide where to load the views using:
+  // Eg: return res.render('home/home.pug')
+  app.set("views", path.join(__dirname, "../web"));
+  app.set("view engine", "pug");
+};
