59c5d7d42786fa9e6a8ddeff1b8c1dc3db1a9e49
Jorge Ferreiro <jorge@ferreiro.me>
2020-11-12 01:21:32 +0100

    Migrate Database into mongodb atlas

--- packages/ferreiro-server/setup/setupDatabase.js
+++ packages/ferreiro-server/setup/setupDatabase.js
@@ -1,19 +1,23 @@
-const debug = require('debug')('ferreiro:setup:database')
-const mongoose = require('mongoose')
-const bluebird = require('bluebird')
+const debug = require("debug")("ferreiro:setup:database");
+const mongoose = require("mongoose");
+const bluebird = require("bluebird");
 
-const env = require('../env')
-const mongoDB = env.MONGODB_URI
+const env = require("../env");
+const mongoDB = env.DB_URI;
 
 module.exports = () => {
-    //Set up default mongoose connection
-    mongoose.Promise = bluebird
-    mongoose.connect(mongoDB, {
-        useMongoClient: true
-    }, (error) => {
-        if (error) {
-            throw error
-        }
-        debug('Successfully connected to MongoDB')
-    })
-}
+  //Set up default mongoose connection
+  mongoose.Promise = bluebird;
+  mongoose.connect(
+    mongoDB,
+    {
+      useMongoClient: true,
+    },
+    (error) => {
+      if (error) {
+        throw error;
+      }
+      debug("Successfully connected to MongoDB");
+    }
+  );
+};
